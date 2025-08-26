169637567e11b99820f3e37a8f81ce40b6ac32ce
s.rajabi <s.rajabi@kavano.co>
2025-08-26 17:00:37 +0330

    .

---
+++ THEME_EN/assets/php/mailer.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "table_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $full_name = $_POST['full_name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    $sql = "INSERT INTO contact_form_entries (full_name, email, subject, message) VALUES (?, ?, ?, ?)";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssss", $full_name, $email, $subject, $message);
+    
+    if ($stmt->execute()) {
+        echo "Your message has been successfully sent.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$stmt->close();
+$conn->close();
+?>
